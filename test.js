60a3ecd2629e039da795308e7e221b137a4dbca1
Rory Fitzpatrick <rory@fanduel.com>
2015-03-17 22:07:24 +0000

    It exists

---
+++ test.js
@@ -0,0 +1,22 @@
+var test = require('tape');
+var parseCacheControl = require('./index');
+
+test('parseCacheControl', function (t) {
+  var header = parseCacheControl('must-revalidate, max-age=3600');
+  t.ok(header);
+  t.equal(header['must-revalidate'], true);
+  t.equal(header['max-age'], 3600);
+
+  header = parseCacheControl('must-revalidate, max-age="3600"');
+  t.ok(header);
+  t.equal(header['must-revalidate'], true);
+  t.equal(header['max-age'], 3600);
+
+  header = parseCacheControl('must-revalidate, b =3600');
+  t.notOk(header);
+
+  header = parseCacheControl('must-revalidate, max-age=a3600');
+  t.notOk(header);
+
+  t.end();
+});
